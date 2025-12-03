a31138ee4f6456908bf8fd504a704a63574bd7c3
Agustí Moll <agusti@gmail.com>
2025-12-03 13:35:11 +0100

    first

--- react-books/src/components/Overlay.jsx
+++ react-books/src/components/Overlay.jsx
@@ -4,10 +4,6 @@ const Overlay = () => {
     return (
         <div className="overlay-text">
             <div>
-                <span className="color-change">
-                    This is placeholder text for a brief overview of my writing. It will eventually
-                    describe the focus, tone, and themes of my work.
-                </span>
             </div>
         </div>
     );
